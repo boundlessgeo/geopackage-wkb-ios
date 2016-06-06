489692d586db77dcf0805cd2dc43404805afadd6
Wes Richardet <wes@tetriscodes.com>
2016-06-06 16:09:37 -0500

    Adding imports to umbrella header

--- wkb-ios/wkb_ios.h
+++ wkb-ios/wkb_ios.h
@@ -9,5 +9,31 @@
 #ifndef wkb_ios_wkb_ios_h
 #define wkb_ios_wkb_ios_h
 
+#import <wkb_ios/WKBGeometry.h>
+#import <wkb_ios/WKBCircularString.h>
+#import <wkb_ios/WKBCompoundCurve.h>
+#import <wkb_ios/WKBCurve.h>
+#import <wkb_ios/WKBCurvePolygon.h>
+#import <wkb_ios/WKBGeometryCollection.h>
+#import <wkb_ios/WKBGeometryEnvelope.h>
+#import <wkb_ios/WKBGeometryTypes.h>
+#import <wkb_ios/WKBLineString.h>
+#import <wkb_ios/WKBMultiCurve.h>
+#import <wkb_ios/WKBMultiLineString.h>
+#import <wkb_ios/WKBMultiPoint.h>
+#import <wkb_ios/WKBMultiPolygon.h>
+#import <wkb_ios/WKBMultiSurface.h>
+#import <wkb_ios/WKBPoint.h>
+#import <wkb_ios/WKBPolygon.h>
+#import <wkb_ios/WKBPolyhedralSurface.h>
+#import <wkb_ios/WKBSurface.h>
+#import <wkb_ios/WKBTIN.h>
+#import <wkb_ios/WKBTriangle.h>
+#import <wkb_ios/WKBByteReader.h>
+#import <wkb_ios/WKBByteWriter.h>
+#import <wkb_ios/WKBGeometryReader.h>
+#import <wkb_ios/WKBGeometryWriter.h>
+#import <wkb_ios/WKBGeometryEnvelopeBuilder.h>
+#import <wkb_ios/WKBGeometryPrinter.h>
 
 #endif
